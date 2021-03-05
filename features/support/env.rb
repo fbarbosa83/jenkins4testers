f8e8510b7a0409cad7536ae35dcf3f85442dd170
fbarbosa83 <felipebarbosa83@yahoo.com.br>
2021-03-05 09:10:12 -0300

    envrb

--- features/support/env.rb
+++ features/support/env.rb
@@ -20,7 +20,7 @@ when "headless"
       options.add_argument "--headless"
       options.add_argument "--disable-gpu"
       options.add_argument "--no-sandbox"
-      options.add_argument "--disable-site-isolation-trials"
+      #options.add_argument "--disable-site-isolation-trials"
     end
     Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
   end
